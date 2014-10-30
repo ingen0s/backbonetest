2dc30ddb6553e9466b45f0a64eb25c8b5f98f12d
Igor Komolov <igor@microsawft.com>
2014-10-30 09:09:13 -0400

    db updates

--- config/bookmarks_database.js
+++ config/bookmarks_database.js
@@ -1,10 +1,9 @@
 // config/bookmarks_database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'port': '3306',
-        'user': 'admin',
-        'password': ''
+        'host': '23.229.203.134',
+        'user': 'abcdefg',
+        'password': '11111'
     },
 	'database': 'taskplosion',
     'users_table': 'bookmarks'
